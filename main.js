f8fe0cc1577286bec250ce015f2fdc439d1ca0ec
Theo Pierson <thheopierson@gmail.com>
2023-06-13 12:34:34 +0200

    build: declare variables

---
+++ main.js
@@ -0,0 +1,5 @@
+"use strict";
+
+let playerOneScore = document.getElementById("playerOne");
+let playerTwoScore = document.getElementById("playerTwo");
+let endScore = document.getElementById("endScore");
