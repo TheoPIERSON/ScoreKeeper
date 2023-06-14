ab4d827c7f2777673f309903a04ca9929dea0b83
Theo Pierson <thheopierson@gmail.com>
2023-06-14 11:11:30 +0200

    fix: improve score tracking in Button click event

--- main.js
+++ main.js
@@ -8,37 +8,40 @@ let endScore = document.getElementById("endScore");
 let resetButton = document.getElementById("resetButton");
 
 let goal = document.getElementById("inputGoal");
+goal.value = Number(goal.value);
 
 let scoreP1 = 0;
 let scoreP2 = 0;
 
-goal.addEventListener("click", function () {
-  endScore.innerHTML = goal.value;
+goal.addEventListener("input", function () {
+  endScore.textContent = goal.value;
 });
 
 playerOneButton.addEventListener("click", function () {
   if (scoreP1 < goal.value && scoreP2 < goal.value) {
     console.log("samarsh");
-    playerOneScore.innerHTML = scoreP1 += 1;
-  } else if (scoreP1 == goal.value) {
-    playerOneScore.style.color = "green";
+    playerOneScore.innerHTML = ++scoreP1; // Augmente d'abord le score, puis met à jour l'affichage
+    if (scoreP1 == goal.value) {
+      playerOneScore.style.color = "green"; // Change la couleur du score si l'objectif est atteint
+    }
   }
 });
 
 playerTwoButton.addEventListener("click", function () {
   if (scoreP2 < goal.value && scoreP1 < goal.value) {
-    console.log("samarshoci");
-    playerTwoScore.innerHTML = scoreP2 += 1;
-  } else if (scoreP2 == goal.value) {
-    playerTwoScore.style.color = "green";
+    console.log("samarsh");
+    playerTwoScore.innerHTML = ++scoreP2;
+    if (scoreP2 == goal.value) {
+      playerTwoScore.style.color = "green";
+    }
   }
 });
 
 resetButton.addEventListener("click", function () {
-  playerOneScore.innerHTML = scoreP1 = 0;
+  playerOneScore.textContent = scoreP1 = 0;
   playerOneScore.style.color = "black";
-  playerTwoScore.innerHTML = scoreP2 = 0;
+  playerTwoScore.textContent = scoreP2 = 0;
   playerTwoScore.style.color = "black";
   goal.value = 0;
-  endScore.innerHTML = 0;
+  endScore.textContent = 0;
 });
